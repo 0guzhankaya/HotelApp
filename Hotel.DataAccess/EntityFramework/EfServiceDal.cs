cff921d7312a8a9dfb6bf64643752eb4ebc23972
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-09 00:44:27 +0300

    002-Generic interfaces, ef classes and generic repository added in DataAccess Layer.

---
+++ Hotel.DataAccess/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,19 @@
+﻿using Hotel.DataAccess.Abstract;
+using Hotel.DataAccess.Concrete;
+using Hotel.DataAccess.Repositories;
+using Hotel.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DataAccess.EntityFramework
+{
+    public class EfServiceDal : GenericRepository<Service>, IServiceDal
+    {
+        public EfServiceDal(Context context) : base(context)
+        {
+        }
+    }
+}
