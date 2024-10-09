e4bdd695a5b3ee77f2b88c1273421965391c7ce5
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-09 13:26:28 +0300

    005-Layout added in UI Layer.

---
+++ Frontend/Hotel.WebUI/Views/AdminLayout/HeadPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel.WebUI.Views.AdminLayout
+{
+    public class HeadPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
