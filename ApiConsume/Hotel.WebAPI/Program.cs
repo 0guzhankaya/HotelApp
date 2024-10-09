018efe13ef10203bee8d026d87f09c8febec0994
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-09 11:47:56 +0300

    004-Controllers added in Presentation Layer.

--- ApiConsume/Hotel.WebAPI/Program.cs
+++ ApiConsume/Hotel.WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using Hotel.Business.Abstract;
+using Hotel.Business.Concrete;
+using Hotel.DataAccess.Abstract;
+using Hotel.DataAccess.Concrete;
+using Hotel.DataAccess.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IStaffDal, EfStuffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
 var app = builder.Build();
 
