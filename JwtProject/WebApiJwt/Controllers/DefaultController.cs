a6b7afa64f8d06e5937788df6c84054b301d71ea
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-23 23:02:14 +0300

    016-Jwt Project added.

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+	public class DefaultController : Controller
+	{
+		[HttpGet("[action]")]
+		public IActionResult Test()
+		{
+			return Ok(new CreateToken().TokenCreate());
+		}
+
+		[Authorize]
+		[HttpGet("[action]")]
+		public IActionResult Test2()
+		{
+			return Ok("Hoş Geldiniz.");
+		}
+	}
+}
