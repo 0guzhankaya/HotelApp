ee47f431fc42f522855857321f5f6846d9782658
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-23 22:10:09 +0300

    015-Rapid Api project added.

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/meta/getCurrency"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "c8d9cb9c3cmsh052b30313558c4ap1674e0jsnb4bf529c45a1" },
+                    { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+                }
+            };
+            
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+        }
+    }
+}
---
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+	public class ImdbController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			List<ApiMovieViewModel> apiMovieViewModel = new List<ApiMovieViewModel>
+			{
+				new ApiMovieViewModel { rank = 1, title = "Movie 1", rating = "8.1" },
+				new ApiMovieViewModel { rank = 2, title = "Movie 2", rating = "8.0" },
+			};
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://imdb-top-1000-movies-series.p.rapidapi.com/byrating"),
+				Headers =
+				{
+					{ "x-rapidapi-key", "c8d9cb9c3cmsh052b30313558c4ap1674e0jsnb4bf529c45a1" },
+					{ "x-rapidapi-host", "imdb-top-1000-movies-series.p.rapidapi.com" },
+				},
+			};
+
+			using (var response = await client.SendAsync(request))
+			{
+				if (response.IsSuccessStatusCode)
+				{
+					var body = await response.Content.ReadAsStringAsync();
+					Console.WriteLine(body); // logged the raw response to check it.
+
+					var apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+				}
+				else
+				{
+					Console.WriteLine($"Error: {response.StatusCode}"); // Log the error.
+				}
+				return View(apiMovieViewModel);
+			}
+		}
+	}
+}
